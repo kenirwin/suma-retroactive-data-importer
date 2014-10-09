5e1ad477d380437577ef06c0c380ef7414f3bfc7
Ken Irwin <kirwin@wittenberg.edu>
2014-10-09 11:20:18 -0400

    updated config-sample.php to match my config.php setup

--- config-sample.php
+++ config-sample.php
@@ -1,9 +1,19 @@
 <?php
+/* Suma variables -- these will appear in the `transaction` table */
+$device = "manual";
+$version = "1.1.0";
+
+/* Debugging variables */
+$debug = true; //false; // set to true to get PHP's native error messages
+$debug_level = E_ALL; // you can change the error reporting level
+
+/* MySQL connect variables */
 $host = "localhost";
 $user = "suma_user";
 $password = "password";
 $database = "suma_live";
 
+/* connect to MySQL */
 $db = mysql_pconnect ($host, $user, $password) || die ("failed to connect to MySQL");
 mysql_select_db ($database) || die ("failed to connect to Suma database");
 ?>
