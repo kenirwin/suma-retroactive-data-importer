df04631e97b168eb98a2bd266bf5b28c75ff7c1b
Ken Irwin <kirwin@wittenberg.edu>
2014-10-09 11:25:41 -0400

    create empty default variables in config-sample for possible future use

--- config-sample.php
+++ config-sample.php
@@ -13,6 +13,12 @@ $user = "suma_user";
 $password = "password";
 $database = "suma_live";
 
+/* Possible future variables */
+//future versions might let you set preselected default value for input
+$activity_defaults = array(); 
+$count_defaults = array();
+
+
 /* connect to MySQL */
 $db = mysql_pconnect ($host, $user, $password) || die ("failed to connect to MySQL");
 mysql_select_db ($database) || die ("failed to connect to Suma database");
