49d3eac1e88b6e21e10f74e152e0fdff7584db7b
Ken Irwin <kirwin@wittenberg.edu>
2014-10-27 16:02:56 -0400

    added new variables to config-sample

--- config-sample.php
+++ config-sample.php
@@ -3,6 +3,12 @@
 $device = "manual";
 $version = "1.1.0";
 
+$allow_direct_submit = true; // when this variable is set to true and the $sumaserver_url is set, the script will submit data directly into Suma. If the url is not set or this variable is set to false, the script will display the JSON data to submit, but will not perform the submission
+
+$sumaserver_url = ""; // full url with no trailing slash, e.g. http://www.example.com/sumaserver, see note about sumaserver security in README.md file
+
+$server_path = ""; // root-relative server path with no trailing slash, e.g. /docs/lib/suma
+
 /* Debugging variables */
 $debug = false; //false; // set to true to get PHP's native error messages
 $debug_level = E_ALL; // you can change the error reporting level
