bdd213c4a73d035a67399ac3f5c62be87db355a2
Ken Irwin <kirwin@wittenberg.edu>
2014-10-28 16:50:20 -0400

    removed unnecessary var $suma_path

--- config-sample.php
+++ config-sample.php
@@ -7,8 +7,6 @@ $allow_direct_submit = true; // when this variable is set to true and the $sumas
 
 $sumaserver_url = ""; // full url with no trailing slash, e.g. http://www.example.com/sumaserver, see note about sumaserver security in README.md file
 
-$suma_path = ""; // root-relative server path with no trailing slash, e.g. /docs/lib/suma
-
 /* Debugging variables */
 $debug = false; //false; // set to true to get PHP's native error messages
 $debug_level = E_ALL; // you can change the error reporting level
