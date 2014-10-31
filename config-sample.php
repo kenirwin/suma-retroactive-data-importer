207b5c4c22aead28650892be6dbdf2f90062cb55
Ken Irwin <kirwin@wittenberg.edu>
2014-10-31 13:45:54 -0400

    removed all refs to MySQL from config file (from v1)

--- config-sample.php
+++ config-sample.php
@@ -1,4 +1,5 @@
 <?php
+
 /* Suma variables -- these will appear in the `transaction` table */
 $device = "Suma-Retro-Data-Importer";
 $version = "1.1.0"; // Suma Client Version (not Suma v) -- ignore this variable
@@ -11,19 +12,4 @@ $sumaserver_url = ""; // full url with no trailing slash, e.g. http://www.exampl
 $debug = false; //false; // set to true to get PHP's native error messages
 $debug_level = E_ALL; // you can change the error reporting level
 
-/* MySQL connect variables */
-$host = "localhost";
-$user = "suma_user";
-$password = "password";
-$database = "suma_live";
-
-/* Possible future variables */
-//future versions might let you set preselected default value for input
-$activity_defaults = array(); 
-$count_defaults = array();
-
-
-/* connect to MySQL */
-$db = mysql_pconnect ($host, $user, $password) || die ("failed to connect to MySQL");
-mysql_select_db ($database) || die ("failed to connect to Suma database");
 ?>
