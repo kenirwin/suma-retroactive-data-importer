41d409e3e8b5600b1619f7bc53d2ba35332ea8d7
Ken Irwin <kirwin@wittenberg.edu>
2014-11-04 13:36:03 -0500

    add documentation styles to style.css

--- documentation.php
+++ documentation.php
@@ -2,16 +2,9 @@
 <head>
 <title>Documentation - Suma Retroactive Data Importer</title>
 <link rel="stylesheet" type="text/css" href="style.css">
-   <style>
-   body { 
- margin: 2em 10em 0em 5em; 
- border: 1px solid #eee
- }
-   h1, h2, h3, h4, h5, h6 { border-bottom: 1px solid #eee; padding-bottom: 0.3em }
-</style>
 </head>
 
-<body>
+<body id="documentation">
 
 <h1>Suma Retroactive Data Importer - Documentation</h1>
 <p><a href="./" class="button">Return to main page</a></p>
