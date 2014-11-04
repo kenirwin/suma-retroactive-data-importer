bd3c9f731836dfade48b108a662f7af1dad4a683
Ken Irwin <kirwin@wittenberg.edu>
2014-11-04 13:32:12 -0500

    styled documentation similar to GitHub

--- documentation.php
+++ documentation.php
@@ -2,8 +2,12 @@
 <head>
 <title>Documentation - Suma Retroactive Data Importer</title>
 <link rel="stylesheet" type="text/css" href="style.css">
-<style>
-   body { margin: .5em; }
+   <style>
+   body { 
+ margin: 2em 10em 0em 5em; 
+ border: 1px solid #eee
+ }
+   h1, h2, h3, h4, h5, h6 { border-bottom: 1px solid #eee; padding-bottom: 0.3em }
 </style>
 </head>
 
